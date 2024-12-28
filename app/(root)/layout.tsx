f2ec3811b70f4314f11e35e69da90643a3638050
Derescio <ddw.web.dev.services@gmail.com>
2024-12-28 17:35:29 -0500

    Updated Root Layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -24,15 +24,13 @@ export default function RootLayout({
 }>) {
     return (
         <>
-            <html lang='en' suppressHydrationWarning>
-                <body className={`${inter.className}`}>
-                    <div className='flex h-screen flex-col'>
-                        <Header />
-                        <main className='flex-1 wrapper'>{children}</main>
-                        <Footer />
-                    </div>
-                </body>
-            </html>
+
+            <div className='flex h-screen flex-col'>
+                <Header />
+                <main className='flex-1 wrapper'>{children}</main>
+                <Footer />
+            </div>
+
         </>
     );
 }
