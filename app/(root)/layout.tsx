0594aa8fdc382cc87d4e9fe29769cf4f3071b65b
Derescio <ddw.web.dev.services@gmail.com>
2024-12-28 18:04:00 -0500

    updated root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,21 +1,8 @@
 import type { Metadata } from "next";
 import '@/assets/styles/globals.css';
-import { Inter } from "next/font/google";
 import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constatnts";
 import Header from "@/components/shared/header";
 import Footer from "@/components/footer";
-
-
-const inter = Inter({ subsets: ["latin"] });
-export const metadata: Metadata = {
-    title: {
-        template: `%s | ${APP_NAME}  `,
-        default: APP_NAME,
-    },
-    description: `${APP_DESCRIPTION}`,
-    metadataBase: new URL(SERVER_URL)
-};
-
 
 export default function RootLayout({
     children,
@@ -24,10 +11,12 @@ export default function RootLayout({
 }>) {
     return (
         <>
-
-            <div className='flex h-screen flex-col'>
+            <div className="flex h-screen flex-col">
                 <Header />
-                <main className='flex-1 wrapper'>{children}</main>
+                <main className="flex-1 wrapper">
+                    {children}
+                </main>
+                test
                 <Footer />
             </div>
 
