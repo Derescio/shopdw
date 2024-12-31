eb645fff57a8dbbdcee29b5809044efc3f8eb612
Derescio <ddw.web.dev.services@gmail.com>
2024-12-31 12:04:06 -0500

    Signup/Register Functionality

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,8 +2,10 @@
 
 
 import { signIn, signOut } from '@/auth';
-import { signInFormSchema } from '../validators';
+import { signInFormSchema, signUpFormSchema } from '../validators';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
+import { prisma } from '@/app/db/prisma';
+import { hashSync } from 'bcrypt-ts-edge';
 
 
 export async function signInWithCredentials(formData: FormData) {
@@ -28,4 +30,43 @@ export async function signInWithCredentials(formData: FormData) {
 
 export async function signOutUser() {
     await signOut();
+}
+
+export async function signUp(prevState: unknown, formData: FormData) {
+    try {
+        const user = signUpFormSchema.parse({
+            name: formData.get('name'),
+            email: formData.get('email'),
+            confirmPassword: formData.get('confirmPassword'),
+            password: formData.get('password'),
+        });
+
+        const plainPassword = user.password;
+
+        user.password = hashSync(user.password, 10);
+
+        await prisma.user.create({
+            data: {
+                name: user.name,
+                email: user.email,
+                password: user.password,
+            },
+        });
+
+        await signIn('credentials', {
+            email: user.email,
+            password: plainPassword,
+        });
+
+        return { success: true, message: 'User created successfully' };
+    } catch (error) {
+        if (isRedirectError(error)) {
+            throw error;
+        }
+
+        return {
+            success: false,
+            message: 'Something went wrong',
+        };
+    }
 }
--- lib/validators.ts
+++ lib/validators.ts
@@ -19,8 +19,31 @@ export const insertProductSchema = z.object({
 });
 
 //Schema for signing up users
-
 export const signInFormSchema = z.object({
     email: z.string().email('Invalid email address'),
     password: z.string().min(6, 'Password must be at least 6 characters'),
 });
+
+
+// SignUp/Register Schema
+export const signUpFormSchema = z.object({
+    name: z.string().min(6, 'Name must be at least 6 characters'),
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
+    confirmPassword: z.string().min(6, 'Confirm password must be at least 6 characters'),
+})
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords don't match",
+        path: ['confirmPassword'],
+
+    })
+
+    //The refine function is used to add custom validation to the schema. 
+    // In this case, we are checking if the password and confirmPassword fields match.
+    //It takes a function that returns a boolean value. 
+    // If the function returns false, the validation will fail.
+    // The path option is used to specify the field that failed the validation.
+    // In this case, the path is confirmPassword.
+    // If the validation fails, the error message will be displayed.
+    // The error message is displayed in the form of an object with a message property.
+    // If the password and confirmPassword fields match, the validation will pass and the program will continue.
