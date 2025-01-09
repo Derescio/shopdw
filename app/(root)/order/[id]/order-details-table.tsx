7bbad2e5a38db0a4e5481db91500ac668715ea2a
Derescio <ddw.web.dev.services@gmail.com>
2025-01-09 13:23:42 -0500

    Implemented Paypal

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -10,13 +10,30 @@ import {
     TableHeader,
     TableRow,
 } from '@/components/ui/table';
-// import { useToast } from '@/hooks/use-toast';
+
+import {
+    PayPalButtons,
+    PayPalScriptProvider,
+    usePayPalScriptReducer,
+} from '@paypal/react-paypal-js';
+
+import { useToast } from '@/hooks/use-toast';
+import {
+    approvePayPalOrder,
+    createPayPalOrder,
+} from '@/lib/actions/order-actions';
 import { formatCurrency, formatDateTime, formatId } from '@/lib/utils';
 import { Order } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const OrderDetailsTable = ({ order }: { order: Order }) => {
+
+const OrderDetailsTable = ({ order,
+    paypalClientId,
+}: {
+    order: Order;
+    paypalClientId: string;
+}) => {
     const {
         shippingAddress,
         orderitems,
@@ -31,8 +48,34 @@ const OrderDetailsTable = ({ order }: { order: Order }) => {
         deliveredAt,
     } = order;
 
-    //const { toast } = useToast();
+    const { toast } = useToast();
+    function PrintLoadingState() {
+        const [{ isPending, isRejected }] = usePayPalScriptReducer();
+        let status = '';
+        if (isPending) {
+            status = 'Loading PayPal...';
+        } else if (isRejected) {
+            status = 'Error in loading PayPal.';
+        }
+        return status;
+    }
+    const handleCreatePayPalOrder = async () => {
+        const res = await createPayPalOrder(order.id);
+        if (!res.success)
+            return toast({
+                description: res.message,
+                variant: 'destructive',
+            });
+        return res.data;
+    };
 
+    const handleApprovePayPalOrder = async (data: { orderID: string }) => {
+        const res = await approvePayPalOrder(order.id, data);
+        toast({
+            description: res.message,
+            variant: res.success ? 'default' : 'destructive',
+        });
+    };
     return (
         <>
             <h1 className='py-4 text-2xl'> Order Number {formatId(order.id)}</h1>
@@ -127,6 +170,22 @@ const OrderDetailsTable = ({ order }: { order: Order }) => {
                                 <div>Total</div>
                                 <div>{formatCurrency(totalPrice)}</div>
                             </div>
+                            {
+                                /* PayPal Payment */
+                            }
+                            {
+                                !isPaid && paymentMethod === 'PayPal' && (
+                                    <div>
+                                        <PayPalScriptProvider options={{ clientId: paypalClientId }}>
+                                            <PrintLoadingState />
+                                            <PayPalButtons
+                                                createOrder={handleCreatePayPalOrder}
+                                                onApprove={handleApprovePayPalOrder}
+                                            />
+                                        </PayPalScriptProvider>
+                                    </div>
+                                )
+                            }
                         </CardContent>
                     </Card>
                 </div>
--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -16,13 +16,15 @@ const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
 
     return (
         <>
-            <OrderDetailsTable order={{
-                ...order, shippingAddress: order.shippingAddress as ShippingAddress,
-            }} />
+            <OrderDetailsTable
+                order={{
+                    ...order,
+                    shippingAddress: order.shippingAddress as ShippingAddress,
+                }}
+                paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
+            />
         </>
     )
 }
 
 export default OrderDetailsPage;
-
-
